f64487b87f776c33e223297a7436afe2d312ab38
JOAQUIN BEJAR <jb@taunais.com>
2024-09-24 18:32:36 +0100

    Refactor auth_example.rs for code readability
    Reformatted function parameters and method chaining for improved readability. Ensured proper alignment and indentation to maintain code consistency and clarity.

--- examples/auth_example.rs
+++ examples/auth_example.rs
@@ -1,21 +1,27 @@
-use std::error::Error;
-use tokio;
-use tracing::{error, info};
-use tradier::utils::logger::setup_logger;
-use tradier::config::base::Config;
 use reqwest::Client;
+use std::error::Error;
+use tracing::{error, info};
+use tradier::config::base::Config;
+use tradier::utils::logger::setup_logger;
 
-async fn verify_authentication(access_token: &str, api_base_url: &str) -> Result<(), Box<dyn Error>> {
+async fn verify_authentication(
+    access_token: &str,
+    api_base_url: &str,
+) -> Result<(), Box<dyn Error>> {
     let client = Client::new();
 
-    let response = client.get(&format!("{}/v1/user/profile", api_base_url))
+    let response = client
+        .get(format!("{}/v1/user/profile", api_base_url))
         .header("Authorization", format!("Bearer {}", access_token))
         .header("Accept", "application/json")
         .send()
         .await?;
 
     if response.status().is_success() {
-        info!("Authentication successful. Response: {:?}", response.text().await?);
+        info!(
+            "Authentication successful. Response: {:?}",
+            response.text().await?
+        );
         Ok(())
     } else {
         Err(format!("Authentication failed. Status: {}", response.status()).into())
@@ -29,7 +35,10 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let config = Config::new();
     info!("Config: {:?}", config);
 
-    let access_token = config.credentials.access_token.clone()
+    let access_token = config
+        .credentials
+        .access_token
+        .clone()
         .expect("Access token not found in configuration");
 
     match verify_authentication(&access_token, &config.rest_api.base_url).await {
