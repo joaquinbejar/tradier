9199f7f79fe68fd9420babec091b94484f7ba6fc
JOAQUIN BEJAR <jb@taunais.com>
2024-09-24 18:32:41 +0100

    Refactor websocket example to use more detailed payload
    Updated the market streaming example to include specific payload struct with symbols, filters, and session settings. Removed unused imports and organized existing ones for better readability and efficiency.

--- examples/auth_websocket_example.rs
+++ examples/auth_websocket_example.rs
@@ -1,16 +1,9 @@
 use std::error::Error;
-use tokio;
 use tracing::{error, info};
-use reqwest::Client;
-use tokio_tungstenite::tungstenite::protocol::Message;
-use serde_json::json;
-use tokio_tungstenite::connect_async;
-use url::Url;
-use tradier::utils::logger::setup_logger;
 use tradier::config::base::Config;
-use tradier::wssession::market::MarketSession;
+use tradier::utils::logger::setup_logger;
 use tradier::wssession::account::AccountSession;
-
+use tradier::wssession::market::{MarketSession, MarketSessionFilter, MarketSessionPayload};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -21,20 +14,37 @@ async fn main() -> Result<(), Box<dyn Error>> {
     loop {
         match MarketSession::new(&config).await {
             Ok(market_session) => {
-                info!("Market streaming wssession created with id: {}", market_session.get_session_id());
-                if let Err(e) = market_session.ws_stream(&["AAPL", "SPY210331C00300000"]).await {
+                info!(
+                    "Market streaming wssession created with id: {}",
+                    market_session.get_session_id()
+                );
+                let payload = MarketSessionPayload {
+                    symbols: vec!["AAPL".to_string(), "MSFT".to_string()],
+                    filter: Some(vec![MarketSessionFilter::QUOTE, MarketSessionFilter::TRADE]),
+                    session_id: market_session.get_session_id().to_string(),
+                    linebreak: Some(true),
+                    valid_only: Some(true),
+                    advanced_details: None,
+                };
+                if let Err(e) = market_session.ws_stream(payload).await {
                     error!("Streaming error: {}. Reconnecting...", e);
                 }
             }
             Err(e) => {
-                error!("Failed to create market streaming wssession: {}. Retrying...", e);
+                error!(
+                    "Failed to create market streaming wssession: {}. Retrying...",
+                    e
+                );
             }
         }
 
         // Example of creating an AccountSession (not used in streaming)
         match AccountSession::new(&config).await {
             Ok(account_session) => {
-                info!("Account wssession created with id: {}", account_session.get_session_id());
+                info!(
+                    "Account wssession created with id: {}",
+                    account_session.get_session_id()
+                );
                 // You can use account_session for account-related operations here
             }
             Err(e) => {
