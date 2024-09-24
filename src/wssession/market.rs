abb00579906f5c68565c49d275b2d88894bf76af
JOAQUIN BEJAR <jb@taunais.com>
2024-09-24 18:32:59 +0100

    Add enhanced market session payload struct and refactor ws_stream
    Introduce MarketSessionPayload struct allowing detailed session configuration including symbols, filters, and optional parameters. Refactor ws_stream method to accept MarketSessionPayload instead of a symbols slice for more flexible message composition.

--- src/wssession/market.rs
+++ src/wssession/market.rs
@@ -1,21 +1,92 @@
 use crate::config::base::Config;
+use crate::constants::TRADIER_WS_BASE_URL;
 use crate::wssession::session::{Session, SessionType};
-use std::error::Error;
 use futures_util::{SinkExt, StreamExt};
-use serde_json::json;
+use serde::{Deserialize, Serialize};
+use std::error::Error;
+use std::fmt::{Display, Formatter};
 use tokio_tungstenite::connect_async;
 use tracing::{error, info};
 use tungstenite::Message;
 use url::Url;
-use reqwest::Client;
-use crate::constants::TRADIER_WS_BASE_URL;
 
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub enum MarketSessionFilter {
     TRADE,
     QUOTE,
     SUMMARY,
     TIMESALE,
-    TRADEX
+    TRADEX,
+}
+
+impl MarketSessionFilter {
+    fn as_str(&self) -> &'static str {
+        match self {
+            MarketSessionFilter::TRADE => "trade",
+            MarketSessionFilter::QUOTE => "quote",
+            MarketSessionFilter::SUMMARY => "summary",
+            MarketSessionFilter::TIMESALE => "timesale",
+            MarketSessionFilter::TRADEX => "tradex",
+        }
+    }
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct MarketSessionPayload {
+    pub symbols: Vec<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub filter: Option<Vec<MarketSessionFilter>>,
+    #[serde(rename = "sessionid")]
+    pub session_id: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub linebreak: Option<bool>,
+    #[serde(rename = "validOnly", skip_serializing_if = "Option::is_none")]
+    pub valid_only: Option<bool>,
+    #[serde(rename = "advancedDetails", skip_serializing_if = "Option::is_none")]
+    pub advanced_details: Option<bool>,
+}
+
+impl MarketSessionPayload {
+    pub fn new(symbols: Vec<String>, session_id: String) -> Self {
+        MarketSessionPayload {
+            symbols,
+            filter: Some(vec![MarketSessionFilter::QUOTE]),
+            session_id,
+            linebreak: Some(true),
+            valid_only: Some(false),
+            advanced_details: Some(false),
+        }
+    }
+
+    pub fn get_message(&self) -> Result<Message, Box<dyn Error>> {
+        let result_payload_json = serde_json::to_value(self);
+        match result_payload_json {
+            Ok(value) => Ok(Message::Text(value.to_string())),
+            Err(e) => {
+                error!("Error parsing message");
+                Err(e.into())
+            }
+        }
+    }
+}
+
+impl Display for MarketSessionPayload {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let filter_str: Vec<String> = self.filter.as_ref().map_or(vec![], |filters| {
+            filters.iter().map(|f| f.as_str().to_string()).collect()
+        });
+
+        write!(
+            f,
+            "MarketSessionPayload {{ symbols: {:?}, filter: {:?}, session_id: {}, linebreak: {}, valid_only: {}, advanced_details: {} }}",
+            self.symbols,
+            filter_str,
+            self.session_id,
+            self.linebreak.map_or("None".to_string(), |v| v.to_string()),
+            self.valid_only.map_or("None".to_string(), |v| v.to_string()),
+            self.advanced_details.map_or("None".to_string(), |v| v.to_string())
+        )
+    }
 }
 
 pub struct MarketSession(Session);
@@ -24,9 +95,7 @@ impl MarketSession {
     pub async fn new(config: &Config) -> Result<Self, Box<dyn Error>> {
         match Session::new(SessionType::Market, config).await {
             Ok(session) => Ok(MarketSession(session)),
-            Err(e) => {
-                Err(format!("Error creating account wssession: {}", e).into())
-            }
+            Err(e) => Err(format!("Error creating market WS session: {}", e).into()),
         }
     }
 
@@ -38,7 +107,7 @@ impl MarketSession {
         self.0.get_websocket_url()
     }
 
-    pub async fn ws_stream(&self, symbols: &[&str]) -> Result<(), Box<dyn Error>> {
+    pub async fn ws_stream(&self, payload: MarketSessionPayload) -> Result<(), Box<dyn Error>> {
         let uri = &format!("{}/v1/markets/events", TRADIER_WS_BASE_URL);
         let url = Url::parse(uri)?;
 
@@ -46,15 +115,8 @@ impl MarketSession {
         let (ws_stream, _) = connect_async(url.as_str()).await?;
         let (mut write, mut read) = ws_stream.split();
 
-        let payload = json!({
-        "symbols": symbols,
-        "filter": ["quote"],
-        "sessionid": self.get_session_id(),
-        "linebreak": true,
-        "validOnly": false
-    });
-
-        write.send(Message::Text(payload.to_string())).await?;
+        let message = payload.get_message()?;
+        write.send(message).await?;
         info!("Sent payload: {}", payload);
 
         while let Some(message) = read.next().await {
@@ -81,4 +143,3 @@ impl MarketSession {
         Ok(())
     }
 }
-
